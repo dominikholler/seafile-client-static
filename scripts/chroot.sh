65e865838d4aac55c8db199663dbef4a92fe4843
user <user@thx.(none)>
2014-01-05 21:42:53 +0100

    some notes in LICENSE and added scripts/chroot.sh

---
+++ scripts/chroot.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sudo /bin/bash
+export LC_ALL=C
+mount -t proc proc proc/
+mount -t sysfs sys sys/
+mount -o bind /dev dev/
+mount -t devpts pts dev/pts/
+cp -a /etc/resolv.conf tmp/natty/etc/resolv.conf
+chroot .
+umount dev/pts
+umount dev
+umount sys
+umount proc
+
