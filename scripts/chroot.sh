5af28b4f4e048c2607d3bc6ad412cc2a5d7c5624
dominikx <github@hollyhome.ath.cx>
2014-01-05 22:28:45 +0100

    fix abs path

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -4,7 +4,7 @@ mount -t proc proc proc/
 mount -t sysfs sys sys/
 mount -o bind /dev dev/
 mount -t devpts pts dev/pts/
-cp -a /etc/resolv.conf tmp/natty/etc/resolv.conf
+cp -a /etc/resolv.conf etc/resolv.conf
 chroot .
 umount dev/pts
 umount dev
